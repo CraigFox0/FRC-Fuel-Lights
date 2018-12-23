1b96c16bc878e2a4765a65cf14a9e82a6174126b
Craig Fox <cefox2001@gmail.com>
2018-12-23 17:15:37 -0500

    Add files via upload

---
+++ Shop_Christmas.ino
@@ -0,0 +1,21 @@
+#define REDPIN 5
+#define GREENPIN 6
+#define BLUEPIN 3
+ 
+#define FADESPEED 1000
+ 
+void setup() {
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+}
+ 
+ 
+void loop() {
+  analogWrite(REDPIN, 255);
+  analogWrite(GREENPIN, 0);
+  delay(FADESPEED);
+  analogWrite(REDPIN, 0);
+  analogWrite(GREENPIN, 255);
+  delay(FADESPEED);
+}
