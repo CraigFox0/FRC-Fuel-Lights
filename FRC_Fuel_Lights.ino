561793f1659d3a22dba1131f0cfb3305803dfdba
Craig Fox <cefox2001@gmail.com>
2019-02-02 11:43:32 -0500

    Update FRC_Fuel_Lights.ino

--- FRC_Fuel_Lights.ino
+++ FRC_Fuel_Lights.ino
@@ -13,6 +13,46 @@ void setup() {
  
  
 void loop() {
+  int redOne = 0;
+  int greenOne = 0;
+  int blueOne = 0;
+  int redTwo = 0;
+  int greenTwo = 0;
+  int blueTwo = 0;
+  int redCurrent = redOne;
+  int greenCurrent = greenOne;
+  int blueCurrent = blueOne;
+  adjustColour(redCurrent, greenCurrent, blueCurrent);
+  
+  while (redCurrent != redTwo || blueCurrent != blueTwo || greenCurrent != greenTwo) {
+    if (redCurrent < redTwo) redCurrent++;
+    else if (redCurrent > redTwo) redCurrent--;
+    
+    if (greenCurrent < greenTwo) greenCurrent++;
+    else if (greenCurrent > greenTwo) greenCurrent--;
+    
+    if (blueCurrent < blueTwo) blueCurrent++;
+    else if (blueCurrent > blueTwo) blueCurrent--;
+
+    adjustColour(redCurrent, greenCurrent, blueCurrent);
+    delay(FADESPEED);
+  }
+  delay(WAITTIME);
+  while (redCurrent != redOne || blueCurrent != blueOne || greenCurrent != greenOne) {
+    if (redCurrent < redOne) redCurrent++;
+    else if (redCurrent > redOne) redCurrent--;
+    
+    if (greenCurrent < greenOne) greenCurrent++;
+    else if (greenCurrent > greenOne) greenCurrent--;
+    
+    if (blueCurrent < blueOne) blueCurrent++;
+    else if (blueCurrent > blueOne) blueCurrent--;
+
+    adjustColour(redCurrent, greenCurrent, blueCurrent);
+    delay(FADESPEED);
+  }
+  delay(WAITTIME);
+  
   int i = 0;
   while(i < 256) {
     adjustColour(i, 0, 255-i);
